2cd37e24367c60d97365e3c5ee8b08e9edfd5be8
Thefrench_Monster <75612366+sofiane-code@users.noreply.github.com>
2022-06-15 16:42:57 +0200

    Add files via upload

---
+++ Script Sofiane Bouljarf- Practical TEST.js
@@ -0,0 +1,26 @@
+db.getCollection("Air").find({} , {fields : 1 ,_id:0})
+
+db.getCollection("Air"). find({} , (
+
+
+db.getCollection("Air").findOne({} ,  {_id:"8", record_timestamp : 1})
+
+
+db.getCollection("Air").find({} , {lib_qual : "Moyen",_id:0})
+
+db.getCollection("Air").find({} , {lib_qual : "Moyen",_id:0})
+
+db.getCollection("Air").find({})
+
+db.Air.find().limit(2)
+ 
+db.getCollection("Air).findOne({} ,  {_id:"0"})
+ 
+db.getCollection("Air), find({"code_qual": {$lt:4}} , {_id:0}) 
+ 
+db.getCollection("Air), find({"code_qual": {$gte:2}} , {_id:0})  
+
+
+db.getCollection("Air), find({"fields.code_pm10" : {$ne:1}})
+
+db.getCollection("Air). find({"fields.code_o3" : {$gt:3}})
